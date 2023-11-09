254a3121393103c8422dd568cb502844b2b9a117
bluehorn07 <bluehornblues@gmail.com>
2023-11-09 23:52:51 +0900

    implt auth api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { AuthLoginUserDto, AuthRegisterUserDto } from './auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private awsCognitoService: AuthService) {}
+
+  @Post('/register')
+  async register(@Body() authRegisterUserDto: AuthRegisterUserDto) {
+    return await this.awsCognitoService.registerUser(authRegisterUserDto);
+  }
+
+  @Post('/login')
+  @UsePipes(ValidationPipe)
+  async login(@Body() authLoginUserDto: AuthLoginUserDto) {
+    return await this.awsCognitoService.authenticateUser(authLoginUserDto);
+  }
+}
